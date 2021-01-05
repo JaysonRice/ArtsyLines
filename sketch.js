cecb92bcfbc16082d8a445dddd722f5ce0d49bd8
AdamSpannbauer <spannbaueradam@gmail.com>
2021-01-05 16:07:29 -0500

    multi noisy balls

--- sketch.js
+++ sketch.js
@@ -1,41 +1,52 @@
+// eslint-disable-next-line import/extensions
 import Ball from './src/ball.js';
 
-let ball;
+const balls = [];
+const nBalls = 3;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  ball = new Ball(100);
+  for (let i = 0; i < nBalls; i += 1) {
+    balls.push(new Ball(random(50, 200)));
+  }
 }
 
-const drawLines = (xGap, yGap, b) => {
-  let x1 = 0;
-  let y1 = 0;
-  for (let y2 = yGap; y2 <= height + yGap; y2 += yGap) {
-    y1 = y2;
-    x1 = 0;
-    for (let x2 = xGap; x2 < width + xGap; x2 += xGap) {
-      let drawY1 = y1;
-      let drawY2 = y2;
+const drawLines = (xGap, yGap, ballsArr) => {
+  for (let y = -height * 0.5; y <= height + height * 0.5; y += yGap) {
+    beginShape();
+    noFill();
+    for (let x = -width * 0.5; x <= width + width * 0.5; x += xGap) {
+      let drawY = y;
 
-      if (dist(x2, y2, b.x, b.y) < b.radius) {
-        drawY2 += yGap * 0.7;
-      }
+      // eslint-disable-next-line no-loop-func
+      ballsArr.forEach((b) => {
+        const dy = yGap * 1.5;
 
-      if (dist(x1, y1, b.x, b.y) < b.radius) {
-        drawY1 += yGap * 0.7;
-      }
+        const d = dist(x, y, b.x, b.y);
+        if (d < b.radius) {
+          const noiseScl = 1 - d / b.radius;
+          const offset = noise(frameCount * 0.05, x, y * 0.5);
+          drawY += map(offset, 0, 1, -dy * noiseScl, dy * noiseScl);
+        }
+      });
 
-      line(x1, drawY1, x2, drawY2);
-      x1 = x2;
+      curveVertex(x, drawY);
     }
+    endShape();
   }
 };
 
 function draw() {
-  stroke(255);
-  background(13, 6, 56);
-  ball.update();
-  drawLines(5, 30, ball);
+  background(100, 100, 255);
+
+  stroke(255, 178, 100);
+  strokeWeight(3);
+
+  balls.forEach((ball) => {
+    ball.update();
+  });
+  drawLines(20, 20, balls);
 }
 
 window.setup = setup;
--- src/ball.js
+++ src/ball.js
@@ -5,12 +5,16 @@ export default class Ball {
     this.y = 0;
 
     this.noiseSeed = random(100);
-    this.noiseStep = 0.005;
+    this.noiseStep = 0.001;
   }
 
   update() {
-    this.x = noise(this.noiseSeed) * width;
-    this.y = noise(this.noiseSeed, 10) * height;
+    this.x = noise(this.noiseSeed);
+    this.y = noise(this.noiseSeed, 10);
+
+    this.x = map(this.x, 0, 1, -width / 3, width + width / 3);
+    this.y = map(this.y, 0, 1, -height / 3, height + height / 3);
+
     this.noiseSeed += this.noiseStep;
   }
 }
